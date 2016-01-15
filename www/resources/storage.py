3ace73e633778c9da98783cf38288b8b23c02dd4
Mepla <soheil.nasirian@gmail.com>
2016-01-15 23:07:35 +0330

    All reasons in responses changed to message.

--- www/resources/storage.py
+++ www/resources/storage.py
@@ -7,7 +7,6 @@ __author__ = 'Mepla'
 
 
 import os
-import uuid
 
 import logging
 from flask import request, make_response, jsonify
@@ -27,7 +26,7 @@ class Storage(Resource):
         if not uploaded_file:
             msg = 'You have not sent a file, a file must be attached.'
             logging.error(msg)
-            return make_response(jsonify({'reason': msg}), 400)
+            return make_response(jsonify({'message': msg}), 400)
 
         directory = configs['STORAGE']['STORAGE_PATH']
         directory = directory.rstrip('/') + '/' + uid
@@ -37,13 +36,13 @@ class Storage(Resource):
                 os.mkdir(directory)
             except Exception as exc:
                 logging.info('Path does not exist for UPLOAD_FOLDER: {}'.format(directory))
-                return make_response(jsonify({'reason': 'Internal server error.'}), 500)
+                return make_response(jsonify({'message': 'Internal server error.'}), 500)
 
         try:
             (file_category, file_format) = uploaded_file.mimetype.split('/')
         except Exception as exc:
             logging.info('Could not parse mimetype of file.')
-            return make_response(jsonify({'reason': 'Uploaded file must have a content-type (Mimetype).'}), 400)
+            return make_response(jsonify({'message': 'Uploaded file must have a content-type (Mimetype).'}), 400)
 
         logging.info('File received: %s/%s', file_category, file_format)
 
@@ -51,13 +50,13 @@ class Storage(Resource):
             uploaded_file.save(os.path.join(directory, filename))
 
         else:
-            return make_response(jsonify({'reason': 'Your file is not supported, please read the documentation and '
+            return make_response(jsonify({'message': 'Your file is not supported, please read the documentation and '
                                                     'send an appropriate file.'}), 400)
 
         if os.path.isfile(directory + '/' + filename):
             return {"path": "/storage/" + filename}
         else:
-            return make_response(jsonify({'reason': 'Failed to convert sent file, check you time and try again.'}), 400)
+            return make_response(jsonify({'message': 'Failed to convert sent file, check you time and try again.'}), 400)
 
 
 class StorageAccess(Resource):
@@ -70,6 +69,6 @@ class StorageAccess(Resource):
         logging.debug('User ({}) requested file: {}'.format(uid, full_path))
 
         if not os.path.isfile(full_path):
-            return make_response(jsonify({'reason': 'The file you requested does not exits.'}), 404)
+            return make_response(jsonify({'message': 'The file you requested does not exits.'}), 404)
         else:
             return send_from_directory(directory, file_id, as_attachment=True)
