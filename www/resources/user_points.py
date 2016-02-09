bf5e0015954c2b9d154fb190f33704ec2b54436b
Mepla <soheil.nasirian@gmail.com>
2016-02-09 19:48:45 +0330

    include_business_info implemented in user point transactions

--- www/resources/user_points.py
+++ www/resources/user_points.py
@@ -1,6 +1,8 @@
 import logging
 from flask_restful.reqparse import RequestParser
 from www.resources.config import configs
+from www.resources.databases.factories import DatabaseFactory
+from www.resources.utilities.helpers import convert_str_query_string_to_bool
 
 __author__ = 'Mepla'
 
@@ -15,6 +17,7 @@ from www import oauth2, db_helper
 class UserPointTransactions(Resource):
     def __init__(self):
         self._accountant = Accountant()
+        self.doc_db = DatabaseFactory().get_database_driver('document/docs')
 
     @oauth2.check_access_token
     @db_helper.handle_aliases
@@ -31,6 +34,7 @@ class UserPointTransactions(Resource):
 
         before = args.get('before')
         after = args.get('after')
+        include_business_info = convert_str_query_string_to_bool(args.get('include_business_info'))
 
         if before and after and before < after:
             msg = {'message': '`before` argument must be greater than or equal to `after`.'}
@@ -46,6 +50,37 @@ class UserPointTransactions(Resource):
 
         try:
             result = self._accountant.get_point_transactions(target_uid, limit=limit, before=before, after=after, user_role=user_role)
+            if include_business_info:
+                business_infos = {}
+                for transaction in result:
+                    creditor = transaction.get('creditor')
+                    debtor = transaction.get('debtor')
+                    if creditor == target_uid:
+                        transaction_bid = debtor
+                    else:
+                        transaction_bid = creditor
+
+                    try:
+                        existing_biz = business_infos.get(transaction_bid)
+                        if not existing_biz:
+                            existing_biz = self.doc_db.find_doc('bid', transaction_bid, doc_type='business', limit=1)
+                            business_infos[transaction_bid] = self.filter_business_info_for_transactions(existing_biz)
+                        transaction['business'] = business_infos.get(transaction_bid)
+                    except Exception as exc:
+                        logging.error('Could not fetch user info in User checkins: {}'.format(exc))
+
             return result
         except DatabaseFindError as exc:
             return {'message': 'Internal server error'}, 500
+
+    def filter_business_info_for_transactions(self, business_info):
+        filtered_info = {}
+        if 'name' in business_info:
+            filtered_info['name'] = business_info['name']
+        if 'address' in business_info:
+            filtered_info['address'] = business_info['address']
+        if 'hrbid' in business_info:
+            filtered_info['hrbid'] = business_info['hrbid']
+        if 'gallery' in business_info:
+            filtered_info['gallery'] = business_info['gallery']
+        return filtered_info
